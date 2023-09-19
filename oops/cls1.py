03160a2ab30f83c55225194f34dad434f14d74fa
karthivedha <145430944+karthivedha@users.noreply.github.com>
2023-09-19 13:02:36 +0530

    Add files via upload

---
+++ oops/cls1.py
@@ -0,0 +1,62 @@
+# class Student:
+#     def __init__(self):
+#         self.name="karthivedha"
+#         self.age=20
+#         self.mark=100
+#         print("hello")
+#
+#     def talk(self):
+#         print("hello i am",self.name)
+#         print("my age is",self.age)
+#         print("marks",self.mark)
+# s=Student()
+# s.talk()
+
+# class Student:
+#     def __init__(self,name,rollno,mark):
+#         self.name=name
+#         self.rollno=rollno
+#         self.mark=mark
+#     def talk(self):
+#         print("hello my name",self.name)
+#         print("roll no",self.rollno)
+#         print("marks",self.mark)
+# s1=Student("jana",1001,80)
+# s2=Student("karthi",1002,90)
+# s1.talk()
+# s2.talk()
+
+# class Cars:
+#     def __init__(self, name, color, cost):
+#         self.name = name
+#         self.color = color
+#         self.cost = cost
+#
+#     def talk(self):
+#         print("car name:", self.name)
+#         print("car color:", self.color)
+#         print("car cost:", self.cost)
+#
+# s=Cars("bmw","white",100000)
+# s.talk()
+
+class Student:
+    def __init__(self,m1,m2,m3):
+        self.m1=m1
+        self.m2=m2
+        self.m3=m3
+
+    def arg(self):
+        return ((self.m1+self.m2+self.m3)/3
+                @classmethod
+        def getschool(cls):
+            return (cls.school
+                    @staticmethod
+            def info():
+                print("this is python class you")
+                jana=students(40,90,70)
+                dharshan=students(100,20,40)
+                print(jana.avg())
+                print(dharshan.avg())
+                print(student.get school())
+                print(student.info())
---
+++ oops/if else interview question.py
@@ -0,0 +1,119 @@
+# a=int(input("enter the number a:"))
+# b=int(input("enter the number b:"))
+# if (a<b):
+#     print("a smallest number")
+# if (b<a):
+#     print("b is smallest")
+from six import b
+
+# cp=int(input("enter the cost price:"))
+# sp=int(input("enter the selling price"))
+# if (cp<sp):
+#     c=(sp-cp)
+#     print("profit","gain",c)
+# if (cp>sp):
+#     c=(cp-sp)
+#     print("loss","ungain",c)
+
+# a=int(input("enter the number:"))
+# b=int(input("enter the number:"))
+# c=int(input("enter the number:"))
+# p=(a+b+c)/2
+# print(p)
+
+# a=int(input("enter the number:"))
+# b=int(input("enter the number:"))
+# p=(a**2+b**2)
+# print(p)
+
+# a=int(input("enter the number:"))
+# b=int(input("enter the number:"))
+# c=int(input("enter the number:"))
+# p=(a**10*b**7)/c**12
+# print(p)
+
+# a=int(input("enter the number:"))
+# b=int(input("enter the number:"))
+# p=a**(1/2)+b**(1/2)
+# print((p))
+
+# a=3
+# p=a**(1/2)
+# print(p)
+
+#biggest of three number
+# a=int(input("enter the number:"))
+# b=int(input("enter the number:"))
+# c=int(input("enter the number:"))
+# if (a>=b and a>c):
+#     print("a is biggest number")
+# elif (b>=a and b>c):
+#      print("b is biggest number")
+# elif (c>=a and c>b):
+#      print("c is biggest number")
+# else:
+#      print("all numbers are equal")
+
+#smallest of four number
+# a=int(input("enter the number A:"))
+# b=int(input("enter the number B:"))
+# c=int(input("enter the number C:"))
+# d=int(input("enter the number D:"))
+# if (a<=b and a<c and a<d):
+#    print("a is smallest number")
+# elif (b<=a and b<c and b<d):
+#     print("b is smallest number")
+# elif (c<=a and c<b and c<d):
+#     print("c is smallest number")
+# elif (d<=a and d<b and d<c):
+#      print(" d is smallest number")
+# else:
+#      print("all numbers are equal")
+
+#total mark and average value
+# name=(input("enter the name:"))
+# a=int(input("tamil:"))
+# b=int(input("english:"))
+# c=int(input("maths:"))
+# d=int(input("science:"))
+# e=int(input("social:"))
+# p=(a+b+c+d+e)
+# print("total",p)
+# average=(p/5)
+# print("average value is",average)
+
+#vowels or constant
+# x=(input("enter the word:"))
+# if (x=='a' or x=='e' or x=='i'or x=='o' or x=='u'):
+#     print("vowels")
+# else:
+#     print("constant")
+
+#grade consider pass or fail
+# a=int(input("enter the grade number:"))
+# if (100>= a >=80):
+#     print("a grade")
+# elif (79>= a >=60):
+#      print("b grade")
+# elif (59>= a >=40):
+#      print("c grade")
+# else:
+#     print("fail the examination")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
